18578ee7347b9ac5199577c5942b9199a1c65faa
artik292 <artik292@gmail.com>
2023-11-06 16:23:10 +0200

    Some fixes

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Переходим в каталог проекта
 cd /var/www/symfony || exit
 
+# Установка зависимостей через Composer
 composer install
 
+# Удаление базы данных, если она существует
 php bin/console doctrine:database:drop --if-exists --force
 
+# Создание базы данных
 php bin/console doctrine:database:create
 
+# Выполнение миграций
 php bin/console --no-interaction --allow-no-migration doctrine:migrations:migrate
+
+# Следующая команда (или команды), которые должны быть выполнены
+# ...
+
+exec "$@"
