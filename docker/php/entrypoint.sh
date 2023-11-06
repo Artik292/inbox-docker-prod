2d2a23d7af43a9b4388018884a05577fce1908e2
artik292 <artik292@gmail.com>
2023-11-06 16:30:42 +0200

    Init commit

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -4,16 +4,16 @@
 cd /var/www/symfony || exit
 
 # Установка зависимостей через Composer
-composer install
+composer update
 
-# Удаление базы данных, если она существует
-php bin/console doctrine:database:drop --if-exists --force
-
-# Создание базы данных
-php bin/console doctrine:database:create
-
-# Выполнение миграций
-php bin/console --no-interaction --allow-no-migration doctrine:migrations:migrate
+## Удаление базы данных, если она существует
+#php bin/console doctrine:database:drop --if-exists --force
+#
+## Создание базы данных
+#php bin/console doctrine:database:create
+#
+## Выполнение миграций
+#php bin/console --no-interaction --allow-no-migration doctrine:migrations:migrate
 
 # Следующая команда (или команды), которые должны быть выполнены
 # ...
---
+++ migrate.sh
@@ -0,0 +1,1 @@
+cd /var/www/symfony && php bin/console doctrine:database:drop --if-exists --force && cd /var/www/symfony && php bin/console doctrine:database:create && php bin/console --no-interaction --allow-no-migration doctrine:migrations:migrate
